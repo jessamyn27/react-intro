7ed8362c8ae610df06441ffe437c4923f41c5be7
Jessamyn27 <jessamyn.mctwigan@gmail.com>
2018-08-15 00:42:33 -0500

    crud and css

--- tunr_django/tunr/urls.py
+++ tunr_django/tunr/urls.py
@@ -6,6 +6,19 @@ from . import views
 #  first part is calling the function in our views second part is what is going in our template which needs to be in a template folder and this object 'artists' will render
 
 urlpatterns = [
+# artist and song index url paths
     path('', views.artist_list, name='artist_list'),
     path('songs/', views.song_list, name='song_list'),
+# artist and song SHOW routes
+    path('artists/<int:pk>', views.artist_detail, name='artist_detail'),
+    path('songs/<int:id>', views.song_detail, name='song_detail'),
+# artist and song CREATE routes
+    path('artists/new', views.artist_create, name='artist_create'),
+    path('songs/new', views.song_create, name='song_create'),
+# artist and song EDIT routes
+    path('artists/<int:pk>/edit', views.artist_edit, name='artist_edit'),
+    path('songs/<int:id>/edit', views.song_edit, name='song_edit'),
+# artist and song DELETE routes
+    path('artists/<int:pk>/delete', views.artist_delete, name='artist_delete'),
+  path('songs/<int:id>/delete', views.song_delete, name='song_delete'),
 ]
--- tunr_django/tunr/views.py
+++ tunr_django/tunr/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # THIS IS LIKE OUR CONTROLLER IN EXPRESS
 
 # Create your views here.
@@ -6,12 +6,82 @@ from .models import Artist, Song
 
 # this is our db query like saying artist.find in mongoose
 
-# we are injecting this indo our db with a template we have not created yet that will render when this function is called
+# we are injecting this into our db with a template we have not created yet that will render when this function is called
 
+# we are rendering a template. The first argument is the request argument, the second is the template that we want to render, and the third is a dictionary with the data we want to send to the view. In this case, that's the artist QuerySet with the key 'artists'.
+
+# artist index route
 def artist_list(request):
     artists = Artist.objects.all()
     return render(request, 'tunr/artist_list.html', {'artists': artists})
 
+# song index route
 def song_list(request):
     songs = Song.objects.all()
     return render(request, 'tunr/song_list.html', {'songs': songs})
+
+# song show route
+def song_detail(request, id):
+  song = Song.objects.get(id=id)
+  return render(request, 'jim/song_detail.html', {'song': song})
+
+# artist show route
+def artist_detail(request, pk):
+    artist = Artist.objects.get(id=pk)
+    return render(request, 'tunr/artist_detail.html', {'artist': artist})
+
+# artist create route for artist_form
+def artist_create(request):
+    if request.method == 'POST':
+        form = ArtistForm(request.POST)
+        if form.is_valid():
+            artist = form.save()
+            return redirect('artist_detail', pk=artist.pk)
+    else:
+        form = ArtistForm()
+    return render(request, 'tunr/artist_form.html', {'form': form})
+
+# song create route for song_form
+def song_create(request):
+  if request.method == 'POST':
+    form = SongForm(request.POST)
+    if form.is_valid():
+      song = form.save()
+      return redirect('song_detail', id=song.id)
+  else:
+    form = SongForm()
+  return render(request, 'jim/song_form.html', {'form': form})
+
+# artist edit route
+def artist_edit(request, pk):
+  artist = Artist.objects.get(pk=pk)
+  if request.method == 'POST':
+    form = ArtistForm(request.POST, instance=artist)
+    if form.is_valid():
+      artist = form.save()
+      return redirect('artist_detail', pk=artist.pk)
+  else:
+    form = ArtistForm(instance=artist)
+  return render(request, 'jim/artist_form.html', {'form': form})
+
+# song edit route
+def song_edit(request, id):
+  song = Song.objects.get(id=id)
+  if request.method == 'POST':
+    form = SongForm(request.POST, instance=song)
+    if form.is_valid():
+      song = form.save()
+      return redirect('song_detail', id=song.id)
+  else:
+    form = SongForm(instance=song)
+  return render(request, 'jim/song_form.html', {'form': form})
+
+# artist delete route
+def artist_delete(request, pk):
+  Artist.objects.get(id=pk).delete()
+  return redirect('artist_list')
+
+# song delete route
+def song_delete(request, id):
+  Song.objects.get(id=id).delete()
+  return redirect('song_list')
